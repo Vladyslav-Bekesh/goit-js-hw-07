8f169f2ac29f6207a1854596efdea6603c8dc02a
Vladyslav <dyadyavadya308@gmail.com>
2023-03-25 17:22:57 +0200

    task 1 problem

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//! gallery find
+const gallery = document.querySelector(".gallery");
+
+//! markup create
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+//! event listener
+
+gallery.addEventListener("click", openBigImage);
+
+//! functions
+
+function openBigImage(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.original}" alt="${event.target.description}" />`
+  );
+  instance.show();
+}
