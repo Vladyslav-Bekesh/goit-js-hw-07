55a678be52177aa89e55cfe228f90686e93b8a2e
Vladyslav <dyadyavadya308@gmail.com>
2023-03-26 18:19:59 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+//! gallery find
+const gallery = document.querySelector(".gallery");
+
+//! markup create
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+//! event listener
+
+gallery.addEventListener("click", openBigImage);
+
+//! functions
+
+function openBigImage(event) {
+  event.preventDefault();
+  const element = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
